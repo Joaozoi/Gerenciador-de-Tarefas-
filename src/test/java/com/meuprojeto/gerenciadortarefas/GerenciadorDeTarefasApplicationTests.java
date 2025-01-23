66fcc97440f9d8cfb6821a46bc8288f228f88b09
João Pedro Zabarella <zabarellaj@gmail.com>
2025-01-23 12:48:48 -0300

    first commit

---
+++ src/test/java/com/meuprojeto/gerenciadortarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.gerenciadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
